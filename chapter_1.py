de000e7bf1c288614037852cbaa306dbe75ca6ec
Bold Batdorj <43408911+Creativerrr@users.noreply.github.com>
2021-09-06 21:54:55 +1000

    chapeter 1

---
+++ chapter_1.py
@@ -0,0 +1,48 @@
+#1 OOP termininology
+
+#1.1 Classify the cards into the correct buckets. Are the statements true or false?
+
+#True
+#Attributes encode the state of an object and are represented by variables.
+#Methods encode behavior of an object and are represented by functions.
+#Encapsulation is a software design practice of the bundling the data and methods that operate on that data.
+
+#False
+#Object is an abstract template describing the general states and behaviors.
+#Object and class are different terms describing  the same concept.
+#A programming language can be either object-orented or procedural, but not both.
+#.columns is an example of method of a DataFrame object. 
+
+#1.2 Exploring object interface
+#The best way to learn how to write object-oriented code is to study the design of existing classes.
+
+#1.2.1 What class does the mystery object have?
+#ANSWER  __main__.Employee
+
+#1.2.2
+# Print the mystery employee's name
+print(mystery.name)
+
+# Print the mystery employee's salary
+print(mystery.salary)
+
+#1.2.3 
+# Print the mystery employee's name
+print(mystery.name)
+
+# Print the mystery employee's salary
+print(mystery.salary)
+
+# Give the mystery employee a raise of $2500
+mystery.give_raise(2500)
+
+# Print the salary again
+print(mystery.salary)
+
+#1.3 Understanding class definitions
+
+def set_count(self, n):
+    mc.set_count(5)
+    mc.count = mc.count + 1
+    mc=MyCounter()
+    
